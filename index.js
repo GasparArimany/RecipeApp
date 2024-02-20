a555840a030b2f0b5ae2ad31228ba9a59fca9de5
Gaspar Arimany <gaspar.arimany@gmail.com>
2024-02-20 18:03:15 -0300

    adds recipe card adds loading indicators

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     categoriesList.innerHTML = "";
 
     categories.forEach((category) => {
-      const listItem = document.createElement("li");
-      listItem.append(category.strCategory);
-      listItem.addEventListener("click", () => {
+      const catListItem = document.createElement("li");
+      catListItem.append(category.strCategory);
+      catListItem.addEventListener("click", () => {
+        const recipesSection = document.getElementById("recipesSection");
+        recipesSection.innerHTML = "Loading...";
         fetch(
           `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`
         )
@@ -17,15 +19,70 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
             return result.json();
           })
           .then((recipes) => {
-            const recipesList = document.getElementById("recipesList");
-            recipesList.innerHTML = "";
+            recipesSection.innerHTML = "";
+            recipesSection.append("Recipes:");
+            const recipesList = document.createElement("ul");
+
             recipes.meals.forEach((recipe) => {
               const recipeListItem = document.createElement("li");
+              recipeListItem.addEventListener("click", () => {
+                const recipeDetailSection = document.getElementById(
+                  "recipeDetailSection"
+                );
+                recipeDetailSection.innerHTML = "Loading...";
+                fetch(
+                  `https://www.themealdb.com/api/json/v1/1/search.php?s=${recipe.strMeal}`
+                )
+                  .then((result) => {
+                    return result.json();
+                  })
+                  .then(({ meals }) => {
+                    const meal = meals[0];
+                    recipeDetailSection.innerHTML = "";
+                    const recipeTitle = document.createElement("p");
+                    recipeTitle.innerHTML = `Recipe Title: ${meal.strMeal}`;
+                    const recipeTypes = document.createElement("p");
+                    recipeTypes.innerHTML = `Meal Types: ${
+                      meal.strTags ? meal.strTags : "No type"
+                    }`;
+                    const recipeIngredientsList = document.createElement("ul");
+
+                    let ingredientCount = 1;
+                    do {
+                      const ingredientKey = `strIngredient${ingredientCount}`;
+                      const measureKey = `strMeasure${ingredientCount}`;
+
+                      if (!meal[ingredientKey] || !meal[measureKey]) {
+                        break;
+                      }
+
+                      const ingredientListItem = document.createElement("li");
+                      ingredientListItem.append(
+                        `${meal[measureKey]} ${meal[ingredientKey]}`
+                      );
+
+                      recipeIngredientsList.append(ingredientListItem);
+                      ingredientCount++;
+                    } while (true);
+
+                    const recipeInstruction = document.createElement("span");
+                    recipeInstruction.innerHTML = `Instructions: ${meal.strInstructions}`;
+
+                    recipeDetailSection.append(
+                      recipeTitle,
+                      recipeTypes,
+                      recipeIngredientsList,
+                      recipeInstruction
+                    );
+                  });
+              });
               recipeListItem.append(recipe.strMeal);
               recipesList.append(recipeListItem);
             });
+
+            recipesSection.append(recipesList);
           });
       });
-      categoriesList.append(listItem);
+      categoriesList.append(catListItem);
     });
   });
